91c357db79354a78d1cb54f516824cc97cca474e
Matthew <lojsamatvej@gmail.com>
2026-05-31 20:06:59 +0300

    iplement work with course and implement custom middleware

--- express_intro/server.js
+++ express_intro/server.js
@@ -1,22 +1,46 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const cors = require('cors')
+const {logger} = require('./middleware/logEvents')
+const { error } = require('console')
 const PORT = process.env.PORT || 3500;
 
 
-app.use(express.urlencoded({extended:false}))
+//! Logger need to be before everything 
+//* custom middleware logger
+app.use(logger)
 
+//* whitelist is a list that shows, what websites can make request for our backend
+const whitelist = ['https://www.google.com', 'http://127.0.0.1:5500', 'http://localhost:3500']
+const corsOptions = {
+    origin: (origin, callback) => {
+        if(whitelist.indexOf(origin) !== -1){
+            callback(null, true) //* enable CORS
+        } else{
+            callback(new Error("Not allowed by CORS")) //* Disable cors
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
+
+//app.use(express.urlencoded({extended:false}))
+
+//app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '/public')))
 
 
 //* app.get('^/$|/index.html') => will gave us index.html file
 //* app.get('^/$|/index(.html)?') => will gave us index.html file, BUT .html will be optionan 
 app.get('/', (req, res) => { //! here '/' is like index page
-    res.sendFile(path.join(__dirname, 'index.html')); //! BUT if file in folder =>
+    res.sendFile(path.join(__dirname, 'views', 'index.html')); //! BUT if file in folder =>
     //! path.join(__dirname, 'folder_name', 'file_name')
 });
 
 app.get(/\/new-page(\.html)?/, (req, res) => { 
-    res.sendFile(path.join(__dirname, 'new-page.html')); //* the second route
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html')); //* the second route
 });
 
 app.get(/\/old-page(\.html)?/, (req, res) => {
@@ -49,6 +73,6 @@ const three = (req, res) => {
 app.get(/\/chain(\.html)?/, [one, two, three])
 
 app.get(/.*/, (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, '404.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
